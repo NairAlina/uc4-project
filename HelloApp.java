b390839377d36c8b0e7b8d6408573e1d3a6164ac
Alina <heyalinanair@gmail.com>
2026-04-21 10:10:30 +0530

    Coded UC5: using enhanced for loop

--- HelloApp.java
+++ HelloApp.java
@@ -7,13 +7,14 @@ public class HelloApp {
             name = "World";
         } else {
             StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
 
-            for (int i = 0; i < args.length; i++) {
-                nameBuilder.append(args[i]);
-
-                if (i < args.length - 1) {
+            for (String n : args) {
+                if (!first) {
                     nameBuilder.append(", ");
                 }
+                nameBuilder.append(n);
+                first = false;
             }
 
             name = nameBuilder.toString();
